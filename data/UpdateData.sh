5eb9c984a2fcc81b50a76e38225cf8355c040c48
Lukas Geyrhofer <lukas.geyrhofer@gmx.at>
2020-03-18 16:23:23 +0100

    make stuff working again

---
+++ data/UpdateData.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wget https://covid.ourworldindata.org/data/total_cases.csv
+wget https://covid.ourworldindata.org/data/total_deaths.csv
+wget https://covid.ourworldindata.org/data/new_cases.csv
+wget https://covid.ourworldindata.org/data/new_deaths.csv
+wget https://covid.ourworldindata.org/data/full_data.csv
